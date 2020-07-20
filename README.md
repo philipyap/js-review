56a294e172164c2077d8110e26935328904e9ef9
Philip Yap <philipyap@Philips-MacBook.local>
2020-07-20 14:21:17 -0700

    [js-review]add readme

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # js-review
 review of javascript
+
+### Example of Data Types
+```javascript
+let myName = 'Philip';
+let myAge = 18;
+let isKool = true;
+const myFriends = ['Max', 'Roy', 'Tom', 'Lee'];
+const dog ={
+    dogName: 'Elmo',
+    breed: 'German Sherperd',
+    dogAge: 2,
+    food: {
+        one: 'kibbles',
+        two: 'chicken breasts',
+        three: 'carrots',
+    },
+    print: function(){
+        console.log('His foods are ' + this.food);
+
+    }
+
+}
+
+dog.gender = 'male';
+
+console.log(myName);
+console.log(myAge);
+console.log(isKool);
+console.log(myFriends);
+console.log(dog);
+console.log(dog.food.two);  
+```
---
+++ app.js
@@ -0,0 +1,61 @@
+let name = 'Philip';
+let age = 18;
+let isCool = true;
+const friends = ['Kevin', 'Molik', 'Jason', 'Frank', 'Asia'];
+
+const tesla = {
+    insdustry: 'Electric Cars',
+    ceo: 'Elon Musk',
+    yearFounded: 2003,
+    foundingMembers: ['Elon', 'JB Straubel', 'Martin'],
+    stockTicker: 'TSLA',
+    vehicles: {
+        vehicle1: 'Model S',
+        vehicle2: 'Model X',
+        vehicle3: 'Model 3',
+    },
+    print: function(){
+        console.log('The ceo tesla is ' + this.ceo); //this. refer to tesla
+    }
+}
+
+tesla.stockPrice = 1643.00;
+
+console.log(name);
+console.log(age);
+console.log(isCool);
+console.log(friends);
+console.log(tesla);
+console.log(tesla.vehicles.vehicle3);
+//or = console.log(tesla['vehicles']['vehicle3']);
+
+tesla.print();
+
+let myName = 'Philip';
+let myAge = 18;
+let isKool = true;
+const myFriends = ['Max', 'Roy', 'Tom', 'Lee'];
+const dog ={
+    dogName: 'Elmo',
+    breed: 'German Sherperd',
+    dogAge: 2,
+    food: {
+        one: 'kibbles',
+        two: 'chicken breasts',
+        three: 'carrots',
+    },
+    print: function(){
+        console.log('His foods are ' + this.food);
+
+    }
+
+}
+
+dog.gender = 'male';
+
+console.log(myName);
+console.log(myAge);
+console.log(isKool);
+console.log(myFriends);
+console.log(dog);
+console.log(dog.food.two);
